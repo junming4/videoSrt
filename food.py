0ee51fdbe52933e31dc14b37d65e406d568da453
laraveljun <xiaojunming@eelly.net>
2019-09-25 22:14:09 +0800

    edt

--- food.py
+++ food.py
@@ -12,12 +12,12 @@ if __name__ == '__main__':
 
 
     path = 'download'
-    name = 'over-easy-egg'
+    name = 'food-scientist'
     file_name = '%s/%s.mp4'%(path,name)
 
     srtPath =  os.getenv('VIDEO_SRT_PATH')
 
-    url = 'https://video.epicurious.com/watch/50-person-prep-challenge-50-people-try-to-make-an-over-easy-egg'
+    url = 'https://video.epicurious.com/watch/4-levels-4-levels-of-steak-amateur-to-food-scientist'
 
     try:
         downloadVideo(url, file_name).run()
@@ -28,6 +28,10 @@ if __name__ == '__main__':
 
         system('mv %s %s'%(srtName,srtPath))
         system('rm -rf %s/*'%path)
+        system('cd %s'%srtPath)
+        system('git add .')
+        system('git commit -a -m "%s"'%name)
+        system('git push"')
 
     except Exception:
         print ("失败")
